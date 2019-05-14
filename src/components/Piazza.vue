776b091149664a2de22c3c708edadfef6d105b29
Nico <ainililia@163.com>
2019-05-14 16:22:48 +0800

    update

--- src/components/Piazza.vue
+++ src/components/Piazza.vue
@@ -5,7 +5,7 @@
             <Button icon="ios-download-outline" type="primary">Upload</Button>
         </div>
         <div class="contains">
-            <ul>
+            <ul id="animation-ul">
                 <animation-list
                 v-for="animation in animations"
                 v-bind:key="animation.name"
@@ -13,28 +13,33 @@
                 ></animation-list>
             </ul>
         </div>
+         <button v-on:click="createAnimation">Add 1</button>
     </div>
 </template>
 <script>
 import Vue from 'vue'
+
 export default {
-  data () {
-    return {
-      animations: [
-        {name: '1', img: 'http://img1.imgtn.bdimg.com/it/u=646689937,1553666340&fm=11&gp=0.jpg'},
-        {name: '2', img: 'http://img2.imgtn.bdimg.com/it/u=3284165984,681808818&fm=26&gp=0.jpg'},
-        {name: '3', img: 'http://img0.imgtn.bdimg.com/it/u=2546714481,3339179299&fm=11&gp=0.jpg'},
-        {name: '4', img: 'http://img2.imgtn.bdimg.com/it/u=907818336,3106913576&fm=11&gp=0.jpg'},
-        {name: '5', img: 'http://img4.imgtn.bdimg.com/it/u=2184930049,3895733545&fm=11&gp=0.jpg'},
-        {name: '6', img: 'http://img1.imgtn.bdimg.com/it/u=646689937,1553666340&fm=11&gp=0.jpg'},
-        {name: '7', img: 'http://img2.imgtn.bdimg.com/it/u=3284165984,681808818&fm=26&gp=0.jpg'},
-        {name: '8', img: 'http://img0.imgtn.bdimg.com/it/u=2546714481,3339179299&fm=11&gp=0.jpg'},
-        {name: '9', img: 'http://img2.imgtn.bdimg.com/it/u=907818336,3106913576&fm=11&gp=0.jpg'},
-        {name: '0', img: 'http://img4.imgtn.bdimg.com/it/u=2184930049,3895733545&fm=11&gp=0.jpg'}
-      ]
+    data () {
+      return {
+        animations: [
+            {name: 1, img: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1557827901478&di=5d6b3fc033fd3dff90ac8da6d63dc530&imgtype=0&src=http%3A%2F%2Fimg0.pconline.com.cn%2Fpconline%2F1403%2F12%2F4428217_09_thumb.jpg'},
+            {name: 2, img: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1557827901478&di=5d6b3fc033fd3dff90ac8da6d63dc530&imgtype=0&src=http%3A%2F%2Fimg0.pconline.com.cn%2Fpconline%2F1403%2F12%2F4428217_09_thumb.jpg'},
+            {name: 3, img: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1557827901478&di=5d6b3fc033fd3dff90ac8da6d63dc530&imgtype=0&src=http%3A%2F%2Fimg0.pconline.com.cn%2Fpconline%2F1403%2F12%2F4428217_09_thumb.jpg'},
+            {name: 4, img: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1557827901478&di=5d6b3fc033fd3dff90ac8da6d63dc530&imgtype=0&src=http%3A%2F%2Fimg0.pconline.com.cn%2Fpconline%2F1403%2F12%2F4428217_09_thumb.jpg'},
+        ],
+        count: 5
+      }
+    },
+    methods: {
+      createAnimation() {
+        this.animations.push({name: this.count ++, img: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1557827901478&di=5d6b3fc033fd3dff90ac8da6d63dc530&imgtype=0&src=http%3A%2F%2Fimg0.pconline.com.cn%2Fpconline%2F1403%2F12%2F4428217_09_thumb.jpg'})
+      }
+    },
+    created () {
+        // this.createAnimation()
     }
   }
-}
 Vue.component('animation-list', {
   props: ['todo'],
   template: `
@@ -45,12 +50,17 @@ Vue.component('animation-list', {
   `
 })
 
+
 </script>
 
 <style scoped>
 .banner {width: 100%; padding: 10px 20px;}
 .banner button {float: right}
 .contains {width: 100%; padding: 10px 20px; margin-top: 20px}
-.contains ul {width: 1240px; overflow: hidden; margin: 0 auto}
-.contains ul li {float: left; width: 300px; overflow: hidden; padding:5px; border: 1px solid #ccc;}
+.contains ul {
+  width: 1280px; 
+  overflow: hidden; 
+  margin: 0 auto; 
+  }
+.contains ul li {float: left; width: 300px; padding:5px; border: 1px solid #ccc;}
 </style>
