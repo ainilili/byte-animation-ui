91b1163c3489de81c461c2b29772afaf7c7ec19a
Nico <ainililia@163.com>
2019-05-14 16:47:48 +0800

    vue-waterfall-easy components

--- src/components/Piazza.vue
+++ src/components/Piazza.vue
@@ -5,39 +5,54 @@
             <Button icon="ios-download-outline" type="primary">Upload</Button>
         </div>
         <div class="contains">
-            <ul id="animation-ul">
-                <animation-list
-                v-for="animation in animations"
-                v-bind:key="animation.name"
-                v-bind:todo="animation"
-                ></animation-list>
-            </ul>
+            <vue-waterfall-easy :imgsArr="imgsArr" @scrollReachBottom="getData"></vue-waterfall-easy>
         </div>
-         <button v-on:click="createAnimation">Add 1</button>
     </div>
 </template>
 <script>
 import Vue from 'vue'
+import vueWaterfallEasy from 'vue-waterfall-easy'
 
 export default {
     data () {
       return {
-        animations: [
-            {name: 1, img: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1557827901478&di=5d6b3fc033fd3dff90ac8da6d63dc530&imgtype=0&src=http%3A%2F%2Fimg0.pconline.com.cn%2Fpconline%2F1403%2F12%2F4428217_09_thumb.jpg'},
-            {name: 2, img: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1557827901478&di=5d6b3fc033fd3dff90ac8da6d63dc530&imgtype=0&src=http%3A%2F%2Fimg0.pconline.com.cn%2Fpconline%2F1403%2F12%2F4428217_09_thumb.jpg'},
-            {name: 3, img: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1557827901478&di=5d6b3fc033fd3dff90ac8da6d63dc530&imgtype=0&src=http%3A%2F%2Fimg0.pconline.com.cn%2Fpconline%2F1403%2F12%2F4428217_09_thumb.jpg'},
-            {name: 4, img: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1557827901478&di=5d6b3fc033fd3dff90ac8da6d63dc530&imgtype=0&src=http%3A%2F%2Fimg0.pconline.com.cn%2Fpconline%2F1403%2F12%2F4428217_09_thumb.jpg'},
+        imgsArr:[
+          {href:'',src:'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1557827901478&di=5d6b3fc033fd3dff90ac8da6d63dc530&imgtype=0&src=http%3A%2F%2Fimg0.pconline.com.cn%2Fpconline%2F1403%2F12%2F4428217_09_thumb.jpg'},
+          {href:'',src:'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1557827901478&di=5d6b3fc033fd3dff90ac8da6d63dc530&imgtype=0&src=http%3A%2F%2Fimg0.pconline.com.cn%2Fpconline%2F1403%2F12%2F4428217_09_thumb.jpg'},
+          {href:'',src:'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1557827901478&di=5d6b3fc033fd3dff90ac8da6d63dc530&imgtype=0&src=http%3A%2F%2Fimg0.pconline.com.cn%2Fpconline%2F1403%2F12%2F4428217_09_thumb.jpg'},
+          {href:'',src:'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1557827901478&di=5d6b3fc033fd3dff90ac8da6d63dc530&imgtype=0&src=http%3A%2F%2Fimg0.pconline.com.cn%2Fpconline%2F1403%2F12%2F4428217_09_thumb.jpg'},
+          {href:'',src:'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1557827901478&di=5d6b3fc033fd3dff90ac8da6d63dc530&imgtype=0&src=http%3A%2F%2Fimg0.pconline.com.cn%2Fpconline%2F1403%2F12%2F4428217_09_thumb.jpg'},
+          {href:'',src:'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1557827901478&di=5d6b3fc033fd3dff90ac8da6d63dc530&imgtype=0&src=http%3A%2F%2Fimg0.pconline.com.cn%2Fpconline%2F1403%2F12%2F4428217_09_thumb.jpg'},
+          {href:'',src:'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1557827901478&di=5d6b3fc033fd3dff90ac8da6d63dc530&imgtype=0&src=http%3A%2F%2Fimg0.pconline.com.cn%2Fpconline%2F1403%2F12%2F4428217_09_thumb.jpg'},
+          {href:'',src:'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1557827901478&di=5d6b3fc033fd3dff90ac8da6d63dc530&imgtype=0&src=http%3A%2F%2Fimg0.pconline.com.cn%2Fpconline%2F1403%2F12%2F4428217_09_thumb.jpg'},
+          {href:'',src:'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1557827901478&di=5d6b3fc033fd3dff90ac8da6d63dc530&imgtype=0&src=http%3A%2F%2Fimg0.pconline.com.cn%2Fpconline%2F1403%2F12%2F4428217_09_thumb.jpg'},
+          {href:'',src:'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1557827901478&di=5d6b3fc033fd3dff90ac8da6d63dc530&imgtype=0&src=http%3A%2F%2Fimg0.pconline.com.cn%2Fpconline%2F1403%2F12%2F4428217_09_thumb.jpg'},
+          {href:'',src:'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1557827901478&di=5d6b3fc033fd3dff90ac8da6d63dc530&imgtype=0&src=http%3A%2F%2Fimg0.pconline.com.cn%2Fpconline%2F1403%2F12%2F4428217_09_thumb.jpg'},
+          {href:'',src:'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1557827901478&di=5d6b3fc033fd3dff90ac8da6d63dc530&imgtype=0&src=http%3A%2F%2Fimg0.pconline.com.cn%2Fpconline%2F1403%2F12%2F4428217_09_thumb.jpg'},
+          {href:'',src:'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1557827901478&di=5d6b3fc033fd3dff90ac8da6d63dc530&imgtype=0&src=http%3A%2F%2Fimg0.pconline.com.cn%2Fpconline%2F1403%2F12%2F4428217_09_thumb.jpg'},
+          {href:'',src:'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1557827901478&di=5d6b3fc033fd3dff90ac8da6d63dc530&imgtype=0&src=http%3A%2F%2Fimg0.pconline.com.cn%2Fpconline%2F1403%2F12%2F4428217_09_thumb.jpg'},
+          {href:'',src:'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1557827901478&di=5d6b3fc033fd3dff90ac8da6d63dc530&imgtype=0&src=http%3A%2F%2Fimg0.pconline.com.cn%2Fpconline%2F1403%2F12%2F4428217_09_thumb.jpg'},
+          {href:'',src:'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1557827901478&di=5d6b3fc033fd3dff90ac8da6d63dc530&imgtype=0&src=http%3A%2F%2Fimg0.pconline.com.cn%2Fpconline%2F1403%2F12%2F4428217_09_thumb.jpg'},
+          {href:'',src:'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1557827901478&di=5d6b3fc033fd3dff90ac8da6d63dc530&imgtype=0&src=http%3A%2F%2Fimg0.pconline.com.cn%2Fpconline%2F1403%2F12%2F4428217_09_thumb.jpg'}
         ],
-        count: 5
+        group: 8,
+        height: '1000px'
       }
     },
     methods: {
       createAnimation() {
         this.animations.push({name: this.count ++, img: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1557827901478&di=5d6b3fc033fd3dff90ac8da6d63dc530&imgtype=0&src=http%3A%2F%2Fimg0.pconline.com.cn%2Fpconline%2F1403%2F12%2F4428217_09_thumb.jpg'})
+      },
+      getData(){
+        this.imgsArr.push({href:'https://www.baidu.com',src:'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1557827901478&di=5d6b3fc033fd3dff90ac8da6d63dc530&imgtype=0&src=http%3A%2F%2Fimg0.pconline.com.cn%2Fpconline%2F1403%2F12%2F4428217_09_thumb.jpg'})
+        this.group ++;
       }
     },
     created () {
-        // this.createAnimation()
+       this.getData();
+    },
+    components: {
+      vueWaterfallEasy
     }
   }
 Vue.component('animation-list', {
@@ -56,11 +71,5 @@ Vue.component('animation-list', {
 <style scoped>
 .banner {width: 100%; padding: 10px 20px;}
 .banner button {float: right}
-.contains {width: 100%; padding: 10px 20px; margin-top: 20px}
-.contains ul {
-  width: 1280px; 
-  overflow: hidden; 
-  margin: 0 auto; 
-  }
-.contains ul li {float: left; width: 300px; padding:5px; border: 1px solid #ccc;}
+.contains{height:1000px; }
 </style>
