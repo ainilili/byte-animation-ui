239117c1efadd7d33a01a74c6b8e22fef04b700b
Nico <ainililia@163.com>
2019-05-16 19:00:04 +0800

    解决Invalid Host header

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -78,6 +78,7 @@ module.exports = new Promise((resolve, reject) => {
       process.env.PORT = port
       // add port to devServer config
       devWebpackConfig.devServer.port = port
+      devWebpackConfig.devServer.disableHostCheck = true
 
       // Add FriendlyErrorsPlugin
       devWebpackConfig.plugins.push(new FriendlyErrorsPlugin({
